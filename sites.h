5ff4817721d2bd92116b10909a33cf925d026806
Ed Marshall <esm@logic.net>
2001-02-13 23:05:58 +0000

    Update the collection of RBL service suggestions.

--- sites.h
+++ sites.h
@@ -11,89 +11,51 @@
    by default. The listings are checked in the order they are listed
    here.
 
-   These listings are provided to you as a convenience; as I find more
-   listings, I'll add them here, but I neither operate nor recommend
-   any of these particular systems. You should make your own decision
-   as to which listings to support by default. Also, RBL-style systems
-   (aside from the MAPS RBL) have a tendancy to move around a bit, so
-   you should check the service websites from time to time to make
-   sure nothing has changed.
+   These listings are provided as a convenience, and examples of how to
+   add new listings. I'll try to keep them updated if at all possible,
+   but you should not rely blindly on these values being correct, nor
+   should you rely on someone else's judgement about the "goodness" of
+   a particular list; test them, and see if their policies suit your
+   tastes. RBL-style systems tend to move around a bit, so you should
+   check the service websites regularly for updates.
 
    In other words, if you use one of these, and the world ends, don't
-   blame me. You're using them at your own risk.
+   blame me. You're using them at your own risk. If they break, you get
+   to keep both pieces.
 
-   You should also visit era eriksson's fine page listing the currently
-   available RBL-alike systems, which will have far more up-to-date
-   information than this file:
+   For more up-to-date information on current RBL-style listings, see:
 
        http://www.iki.fi/era/rbl/rbl.html
 */
 
+/* dev.null.dk
+   http://dev.null.dk/ (Maybe?) */
+/* SITE("dev.null.dk") */
 
 /* MAPS RBL: Mail Abuse Protection System Realtime Blackhole List
-   http://maps.vix.com/rbl/
+   http://mail-abuse.org/rbl/ */
+/* SITE("blackholes.mail-abuse.org") */
 
-   This listing is human-operated, and is one of the most popular
-   blacklists of spam-originating systems. Originallys started by
-   Paul Vixie, this is the original RBL system. */
-/* SITE("rbl.maps.vix.com") */
+/* MAPS RSS: Mail Abuse Protection System Relay Spam Stopper
+   http://mail-abuse.org/rss/ */
+/* SITE("relays.mail-abuse.org") */
 
+/* MAPS DUL: Mail Abuse Protection System Dial-up User List
+   http://mail-abuse.org/dul/ */
+/* SITE("dialups.mail-abuse.org") */
+
+/* MBL: Mike's Black List
+   http://groups.google.com/groups?ic=1&th=4d82c61a4da48d0a */
+/* SITE("3y.spam.mrs.kithrup.com") */
 
 /* ORBS: Open Relay Behaviour-modification System
    http://www.orbs.org/
-
-   This is an automated listing of IP addresses that have been tested
-   to host an SMTP server that will transmit at least one message to
-   any system for anyone (an "open email relay"). Mechanisms are also
-   available to remove systems that have closed the relay. */
-/* SITE("relays.orbs.org") */
-
-
-/* IMRSS: The Internet Mail Relay Services Survey
-   http://www.imrss.org/
-
-   This list appears to be similar in scope to ORBS, in that it
-   catalogues open mail relays. */
-/* SITE("mr-out.imrss.org") */
-
-
-/* RRSS: The Radparker Relay Spam Stopper
-   http://relays.radparker.com/
-
-   Like ORBS and IMRSS, this system is a listing of open relays, but
-   the maintainer manually handles addition of relays, and only after
-   evidence of spam from that server has been presented. */
-/* SITE("relays.radparker.com") */
-
-
-/* MAPS DUL: Mail Abuse Protection System Dial-up User List
-   http://maps.vix.com/dul/
-
-   This list was started from a list compiled by "Afterburner" (the
-   head-honcho abuse guy at Erols Internet), and extended to be easily
-   added to over time. This service was originally called the ORCA DUL,
-   and was operated at "dul.orca.bc.ca"; the MAPS project invited Al
-   Iverson, the DUL maintainer, to host it on their systems. */
-/* SITE("dul.maps.vix.com") */
-
-
-/* DSSL: The Direct Spam Sources List
-   http://www.imrss.org/dssl/
-
-   This list is similar in intent to the MAPS DUL, but uses a unique
-   scheme of matching patterns to dialup IP naming conventions. The
-   server looks up the IP you pass it, "compresses" the name, and
-   matches it against a series of patterns, returning true or false
-   based on a successful or failed match. */
-/* SITE("dssl.imrss.org") */
-
-/* The Shub-Internet Blackholes
-   http://www.shub-inter.net/
-
-   These listing is human-operated, and is much more aggressive than
-   the MAPS list. If a system appears on either RBL, DUL, or ORBS, it
-   is removed from the listings, and so these should be checked last.
-   The first list (relayips) is a listing of open email relays, and
-   the second (spamips) is a listing of hand-picked spam sources. */
-/* SITE("relayips.shub-inter.net") */
-/* SITE("spamips.shub-inter.net") */
+/* SITE("inputs.orbs.org") */
+/* SITE("outputs.orbs.org") */
+/* SITE("delayed-outputs.orbs.org") */
+/* SITE("manual.orbs.org") */
+/* SITE("spamsources.orbs.org") */
+/* SITE("untestable-netblocks.orbs.org") */
+/* SITE("spamsource-netblocks.orbs.org") */
+/* SITE("testing.orbs.org") */
+/* SITE("ok.orbs.org") */
