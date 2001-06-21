95b56baba7b79b4db4585196ebabd3ffa40b13d1
era eriksson <era@users.sourceforge.net>
2001-06-21 08:58:27 +0000

    Re-adding my own copy -- couldn't get automake build to work without this.

---
+++ acconfig.h
@@ -0,0 +1,6 @@
+/* Comment for acconfig.h only: These #undefs will (hopefully :-) turn into
+   #defines in the resulting config.h during the automake-autoconf build
+   process. */
+
+#undef PACKAGE
+#undef VERSION
