914aa4b45004f8463365aba5edcdceda737ba1e9
Ed Marshall <esm@logic.net>
2001-12-29 06:50:58 +0000

    Removed files which are autogenerated by the development build environment.

--- acconfig.h
+++
@@ -1,6 +0,0 @@
-/* Comment for acconfig.h only: These #undefs will (hopefully :-) turn into
-   #defines in the resulting config.h during the automake-autoconf build
-   process. */
-
-#undef PACKAGE
-#undef VERSION
